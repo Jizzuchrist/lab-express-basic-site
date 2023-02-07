26f70b73ec614734d94bd0d3b2a731332b3ff788
Jizzus <miguel.angelo.jesus@hotmail.com>
2023-02-07 23:19:40 +0000

    done what i could

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 // link the folder with the entire app
-app.use(express.static("public"))
-app.get("/home", (request,response) => {
-    response.sendFile(__dirname + "/views/index.html");
-})
-app.get("/profile", (request,response) => {
-    response.sendFile(__dirname + "/views/profile.html");
-})
+app.use(express.static("public"));
 
-app.listen(3000)
+app.get("/home", (request, response) => {
+  response.sendFile(__dirname + "/views/home.html");
+});
+app.get("/about", (request, response) => {
+  response.sendFile(__dirname + "/views/about.html");
+});
+app.get("/photogallery", (request, response) => {
+  response.sendFile(__dirname + "/views/photogallery.html");
+});
+app.get("/works", (request, response) => {
+  response.sendFile(__dirname + "/views/works.html");
+});
+
+app.listen(3000);
+
---
+++ public/script.js
@@ -0,0 +1,28 @@
+/* let count = 0;
+function lbuttonClick() {
+  count -= 1;
+  if (count === 0) {
+    document.getElementsByClassName("arrow-left").classList.add("hidden");
+    document.getElementsByClassName("section1").classList.remove("hidden");
+  } else if (count === 1) {
+    document.getElementsByClassName("section1").classList.add("hidden");
+    document.getElementsByClassName("arrow-left").classList.remove("hidden");
+    document.getElementsByClassName("arrow-right").classList.remove("hidden");
+  } else if (count === 2) {
+    document.getElementsByClassName("arrow-right").classList.add("hidden");
+  }
+}
+
+function rbuttonOnclick() {
+  count += 1;
+  if (count === 0) {
+    document.getElementsByClassName("arrow-left").classList.add("hidden");
+    document.getElementsByClassName("childhood").classList.remove("hidden");
+  } else if (count === 1) {
+    document.getElementsByClassName("section1").classList.add("hidden");
+    document.getElementsByClassName("arrow-right").classList.remove("hidden");
+    document.getElementsByClassName("arrow-left").classList.remove("hidden");
+  } else if (count === 2) {
+    document.getElementsByClassName("arrow-right").classList.add("hidden");
+  }
+} */
